fa014a34106e99e11b33b5ceca14b325ce2bf526
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-03-06 08:43:24 -0400

    ultraelf.lisp: Lisätty x86-64-rekisterilistoja.
    Lisätty x86-64-rekisterilistat:
    `defparameter *old-low-reg8-list*`,
    `defparameter *old-high-reg8-list*`,
    `defparameter *new-low-reg8-list*`,
    `defparameter *old-reg8-list*`,
    `defparameter *low-reg8-list*`,
    `defparameter *reg8-list*`,
    `defparameter *old-reg16-list*`,
    `defparameter *new-reg16-list*`,
    `defparameter *reg16-list*`,
    `defparameter *old-reg32-list*`,
    `defparameter *new-reg32-list*`,
    `defparameter *reg32-list*`,
    `defparameter *old-reg64-list*`,
    `defparameter *new-reg64-list*` &
    `defparameter *reg64-list*`.
    
    Ei muita muutoksia.
    
    	modified:   ultraelf.lisp

--- ultraelf.lisp
+++ ultraelf.lisp
@@ -170,6 +170,25 @@
 (defun std ()
   #xfd)
 
+(defparameter *old-low-reg8-list*  (list  "al"  "cl"  "dl"  "bl"))
+(defparameter *old-high-reg8-list* (list  "ah"  "ch"  "dh"  "bh"))
+(defparameter *new-low-reg8-list*  (list "spl" "bpl" "sil" "dil" "r8l" "r9l" "r10l" "r11l" "r12l" "r13l" "r14l" "r15l"))
+(defparameter *old-reg8-list* (append *old-low-reg8-list* *old-high-reg8-list*))
+(defparameter *low-reg8-list* (append *old-low-reg8-list* *new-low-reg8-list*))
+(defparameter *reg8-list* (append *old-low-reg8-list* *old-high-reg8-list* *new-low-reg8-list*))
+
+(defparameter *old-reg16-list* (list  "ax"  "cx"   "dx"   "bx"   "sp"   "bp"   "si"   "di"))
+(defparameter *new-reg16-list* (list "r8w" "r9w" "r10w" "r11w" "r12w" "r13w" "r14w" "r15w"))
+(defparameter *reg16-list* (append *old-reg16-list* *new-reg16-list*))
+
+(defparameter *old-reg32-list* (list "eax" "ecx"  "edx"  "ebx"  "esp"  "ebp"  "esi"  "edi"))
+(defparameter *new-reg32-list* (list "r8d" "r9d" "r10d" "r11d" "r12d" "r13d" "r14d" "r15d"))
+(defparameter *reg32-list* (append *old-reg32-list* *new-reg32-list*))
+
+(defparameter *old-reg64-list* (list "rax" "rcx" "rdx" "rbx" "rsp" "rbp" "rsi" "rdi"))
+(defparameter *new-reg64-list* (list  "r8"  "r9" "r10" "r11" "r12" "r13" "r14" "r15"))
+(defparameter *reg64-list* (append *old-reg64-list* *new-reg64-list*))
+
 (defparameter *example-code*
   #a
   mul  rax     ; rdx:rax = rax^2.
