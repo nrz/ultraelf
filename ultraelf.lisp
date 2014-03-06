dcbe0256f5426d8f2daec43dfae83aecdf34ef2a
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-03-06 08:38:16 -0400

    ultraelf.lisp: Lisätty `defun print-hex`.
    Ei muita muutoksia.
    
    	modified:   ultraelf.lisp

--- ultraelf.lisp
+++ ultraelf.lisp
@@ -114,6 +114,9 @@
   "This function converts a string produced by transform-code-to-string into a syntax tree."
   (read-from-string my-string))
 
+(defun print-hex (my-number)
+  (format nil "~x" my-number))
+
 (defun string-to-function (my-string)
   "This fnuction converts a string to a function.
    http://stackoverflow.com/questions/2940267/call-function-based-on-a-string/2940347#2940347"
