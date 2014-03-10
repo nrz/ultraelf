ec51379c11943f903debdf1c3d670275e5dbce9f
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-03-10 10:56:23 -0400

    ultraelf.lisp: `defparameter *sreg2-hash-table-x64*`.
    Ei muita muutoksia.
    
    	modified:   ultraelf.lisp

--- ultraelf.lisp
+++ ultraelf.lisp
@@ -466,6 +466,12 @@
 (setf (gethash "4" *modrm-reg-hash-table*) #b10)
 (setf (gethash "8" *modrm-reg-hash-table*) #b11)
 
+(defparameter *sreg2-hash-table-x64* (make-hash-table :test 'equalp))
+(setf (gethash "es" *sreg2-hash-table-x64*) #b00)
+(setf (gethash "cs" *sreg2-hash-table-x64*) #b01)
+(setf (gethash "ss" *sreg2-hash-table-x64*) #b10)
+(setf (gethash "ds" *sreg2-hash-table-x64*) #b11)
+
 (defparameter *old-low-reg8-list*  (list  "al"  "cl"  "dl"  "bl"))
 (defparameter *old-high-reg8-list* (list  "ah"  "ch"  "dh"  "bh"))
 (defparameter *new-low-reg8-list*  (list "spl" "bpl" "sil" "dil" "r8l" "r9l" "r10l" "r11l" "r12l" "r13l" "r14l" "r15l"))
