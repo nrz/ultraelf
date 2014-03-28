88179b1ed12ddcf9759652c8ceb8eb37020798f0
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-03-28 11:34:36 +0200

    ultraelf-x86-general-emit-functions.lisp: muistiosoituskoodia (SIB).
    `defun get-base-index-and-scale`:
    * Erottelee muistiosoitussyntaksista kannan (base), indeksin (index) ja
      kertoimen (scale). Toistaiseksi ei tee tarkistuksia.
    
    `defun emit-sib-byte`:
    * Tuottaa listan, joka sisältää ainoastaan SIB-tavun, syötteenä `base`,
      `index` & `scale`.
    
    `defun emit-sib-byte-for-memory-address-syntax`:
    * Tuottaa listan, joka sisältää ainoastaan SIB-tavun, syötteenä
      `memory-address-syntax`.
    
    Ei muita muutoksia.
    
    	modified:   ultraelf-x86-general-emit-functions.lisp

--- ultraelf-x86-general-emit-functions.lisp
+++ ultraelf-x86-general-emit-functions.lisp
@@ -5,6 +5,50 @@
 
 (in-package :ultraelf)
 
+(defun get-base-index-and-scale (memory-address-syntax)
+  "This function converts memory address syntax to a list of base, index and scale.
+   Memory address syntax must start with [ and end with ] and it must not contain spaces."
+  (let*
+    ((base-index-and-scale (list "" "" ""))
+     (list-index 0)
+     (memory-address-syntax-list
+       (cdr
+         (split-string-into-list-of-strings
+           (get-string-without-last-character memory-address-syntax)))))
+    (loop for my-char in memory-address-syntax-list
+          do (cond
+               ((equal my-char "+")
+                (incf list-index))
+               ((equal my-char "*")
+                (incf list-index))
+               (t (setf (nth list-index base-index-and-scale)
+                        (concatenate 'string (nth list-index base-index-and-scale) my-char)))))
+    ;; if all base, index and scale are defined, fix the order before returning it.
+    ;; otherwise set scale to 1 and return the list.
+    (if (> (length (nth 2 base-index-and-scale)) 0)
+      (return-from get-base-index-and-scale (list (nth 0 base-index-and-scale)
+                                                  (nth 2 base-index-and-scale)
+                                                  (nth 1 base-index-and-scale)))
+      (return-from get-base-index-and-scale (list (nth 0 base-index-and-scale)
+                                                  (nth 1 base-index-and-scale)
+                                                  "1")))))
+
+(defun emit-sib-byte (base index scale)
+  "This function emits SIB byte."
+  (list (logior
+          (gethash base *modrm-reg-hash-table-x64*)
+          (ash (gethash index *modrm-reg-hash-table-x64*) 3)
+          (ash (gethash scale *sib-scale-hash-table-x64*) 6))))
+
+(defun emit-sib-byte-for-memory-address-syntax (memory-address-syntax)
+  "This function emits SIB byte for a memory address syntax."
+  (let*
+    ((base-index-and-scale nil))
+    (setf base-index-and-scale (get-base-index-and-scale memory-address-syntax))
+    (emit-sib-byte (nth 0 base-index-and-scale)
+                   (nth 1 base-index-and-scale)
+                   (nth 2 base-index-and-scale))))
+
 (defun one-operand-x64 (first-byte-base reg-byte-base arg1 &optional arg2) 
   (let*
     ((modrm (gethash arg1 *modrm-reg-hash-table-x64*)))
