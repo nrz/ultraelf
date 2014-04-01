cc426f1e650aaa889bb91a0d74f34111309f7c5e
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-04-01 11:14:01 +0300

    ultraelf-alt-code.lisp: Lisätty, muokattu ja kommentoitu alt-koodia.
    Ei muita muutoksia.
    
    	modified:   ultraelf-alt-code.lisp

--- ultraelf-alt-code.lisp
+++ ultraelf-alt-code.lisp
@@ -18,6 +18,48 @@
   lea   arg1,[arg2]
   #e)
 
+(defparameter *alt-lea-reg64-m32-mov*
+  ;; clean, does not modify flags.
+  #a
+  mov   arg1,arg2
+  #e)
+
+(defparameter *alt-lea-reg64-indirect-reg64-mov*
+  ;; clean, does not modify flags.
+  #a
+  mov   arg1,arg2
+  #e)
+
+(defparameter *alt-push-reg64-lea-mov*
+  #a
+  lea   rsp,[rsp-8]
+  mov   [rsp],arg1
+  #e)
+
+(defparameter *alt-push-imm8-lea-mov*
+  #a
+  lea   rsp,[rsp-8]
+  mov   [rsp],qword arg1
+  #e)
+
+(defparameter *alt-push-imm16-lea-mov*
+  #a
+  lea   rsp,[rsp-8]
+  mov   [rsp],qword arg1
+  #e)
+
+(defparameter *alt-push-imm32-lea-mov*
+  #a
+  lea   rsp,[rsp-8]
+  mov   [rsp],qword arg1
+  #e)
+
+(defparameter *alt-pop-reg64-lea-mov-lea*
+  #a
+  mov   arg1,[rsp]
+  lea   rsp,[rsp+8]
+  #e)
+
 (defparameter *alt-add-reg64-reg64-lea*
   ;; clean, does not modify flags.
   ;; overflow?
@@ -44,12 +86,14 @@
   #e)
 
 (defparameter *alt-zero-reg-xor*
+  ;; not clean.
   ;; modifies flags.
   #a
   xor   arg1,arg1
   #e)
 
 (defparameter *alt-zero-reg-sub*
+  ;; not clean.
   ;; modifies flags.
   #a
   sub   arg1,arg1
@@ -77,18 +121,53 @@
   pop   arg1
   #e)
 
-(defparameter *alt-or-and-reg-reg-test*
+(defparameter *alt-and-reg1-reg1-test*
+  ;; clean, modifies flags identically.
   #a
   test  arg1,arg1
   #e)
 
-(defparameter *alt-test-reg-reg-or*
+(defparameter *alt-or-reg1-reg1-test*
+  ;; clean?
+  #a
+  test  arg1,arg1
+  #e)
+
+(defparameter *alt-test-reg1-reg1-or*
+  ;; clean?
   #a
   or    arg1,arg1
   #e)
 
-(defparameter *alt-test-reg-reg-and*
+(defparameter *alt-test-reg1-reg1-and*
+  ;; clean, modifies flags identically.
   #a
   and   arg1,arg1
   #e)
 
+(defparameter *alt-call-push-jmp*
+  ;; clean.
+  ;; uses forward reference to a local label.
+  #a
+  push  qword >.label
+  jmp   arg1
+  .label:
+  #e)
+
+(defparameter *alt-ret-ret-0*
+  ;; clean.
+  #a
+  ret   0
+  #e)
+
+(defparameter *alt-ret-near-ret-near-0*
+  ;; clean.
+  #a
+  ret   near 0
+  #e)
+
+(defparameter *alt-ret-far-ret-far-0*
+  ;; clean.
+  #a
+  ret   far 0
+  #e)
