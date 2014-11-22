ea0cca8c557881596b4fd2442cf73b2db5a89635
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-11-22 12:35:56 +0200

    Lisätty `(in-package :ultraelf)` ja alun kommentit.
    Jatkossa tähän täytyy lukea alkuperäisen `insns.dat`:n copyright-teksti
    jne.
    
    	modified:   insns.lisp

--- insns.lisp
+++ insns.lisp
@@ -1,3 +1,10 @@
+;;;; ultraELF 0.0.1
+;;;
+;;; ultraELF x86-64 assembler, disassembler and metamorphic engine.
+;;; ultraELF packs and reconstructs ELF executables, maintaining original functionality.
+
+(in-package :ultraelf)
+
 (defparameter *x86-instructions-list-from-nasm* (list
 (list "DB"               "ignore"                                     "ignore"                                         "ignore"                    )
 (list "DW"               "ignore"                                     "ignore"                                         "ignore"                    )
