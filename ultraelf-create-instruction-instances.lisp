7c52706716143f99dfdf9877bed13fef91d40175
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-11-29 12:10:49 +0200

    Muokattu kommentteja.
    Muutos tehty sed:n regexillä:
    
    `find . -iname '*.lisp' | xargs -I{} ` ...
        `sed -i 's/\(ultraELF \)\(x86-\)64/\1\216, \232, \264 \& ARM/g' {}`.
    
    	modified:   perl/ultraelf-lisp-file-header.lisp
    	modified:   ultraelf-addressing-form-classes.lisp
    	modified:   ultraelf-addressing-form-lists.lisp
    	modified:   ultraelf-alt-code.lisp
    	modified:   ultraelf-architecture-classes.lisp
    	modified:   ultraelf-argument-classes.lisp
    	modified:   ultraelf-arm-assembling-functions.lisp
    	modified:   ultraelf-arm-instruction-classes.lisp
    	modified:   ultraelf-arm-instructions.lisp
    	modified:   ultraelf-arm.lisp
    	modified:   ultraelf-asm-pseudo-ops.lisp
    	modified:   ultraelf-asm-reader.lisp
    	modified:   ultraelf-assembling-functions.lisp
    	modified:   ultraelf-compiling-macros.lisp
    	modified:   ultraelf-create-addressing-form-instances.lisp
    	modified:   ultraelf-create-instruction-instances.lisp
    	modified:   ultraelf-elf-classes.lisp
    	modified:   ultraelf-essentials.lisp
    	modified:   ultraelf-general-emit-functions.lisp
    	modified:   ultraelf-init.lisp
    	modified:   ultraelf-instruction-classes.lisp
    	modified:   ultraelf-instruction-hash-tables.lisp
    	modified:   ultraelf-instruction-lists.lisp
    	modified:   ultraelf-instruction-package.lisp
    	modified:   ultraelf-lisp-file-header.lisp
    	modified:   ultraelf-numbers.lisp
    	modified:   ultraelf-package.lisp
    	modified:   ultraelf-printing.lisp
    	modified:   ultraelf-regex.lisp
    	modified:   ultraelf-sequence.lisp
    	modified:   ultraelf-sib.lisp
    	modified:   ultraelf-sreg2.lisp
    	modified:   ultraelf-sreg3.lisp
    	modified:   ultraelf-string.lisp
    	modified:   ultraelf-test-x64-code.lisp
    	modified:   ultraelf-x16-assembling-functions.lisp
    	modified:   ultraelf-x16-instruction-classes.lisp
    	modified:   ultraelf-x16.lisp
    	modified:   ultraelf-x32-assembling-functions.lisp
    	modified:   ultraelf-x32-instruction-classes.lisp
    	modified:   ultraelf-x32.lisp
    	modified:   ultraelf-x64-assembling-functions.lisp
    	modified:   ultraelf-x64-instruction-classes.lisp
    	modified:   ultraelf-x64-rex.lisp
    	modified:   ultraelf-x64.lisp
    	modified:   ultraelf-x86-addressing-form-instances.lisp
    	modified:   ultraelf-x86-general-emit-functions.lisp
    	modified:   ultraelf-x86-instruction-classes.lisp
    	modified:   ultraelf-x86-instructions.lisp
    	modified:   ultraelf-x87-instructions.lisp

--- ultraelf-create-instruction-instances.lisp
+++ ultraelf-create-instruction-instances.lisp
@@ -1,6 +1,6 @@
 ;;;; ultraELF 0.0.1
 ;;;
-;;; ultraELF x86-64 assembler, disassembler and metamorphic engine.
+;;; ultraELF x86-16, x86-32, x86-64 & ARM assembler, disassembler and metamorphic engine.
 ;;; ultraELF packs and reconstructs ELF executables, maintaining original functionality.
 
 (in-package :x64)
