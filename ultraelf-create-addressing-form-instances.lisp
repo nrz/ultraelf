6d07e8cd9e0ee811d7ee20e0592f0a18e8315f23
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-12-07 15:21:36 +0200

    Poistettu tarpeeton `eval-when`.
    	modified:   ultraelf-create-addressing-form-instances.lisp

--- ultraelf-create-addressing-form-instances.lisp
+++ ultraelf-create-addressing-form-instances.lisp
@@ -8,10 +8,9 @@
 ;; create addressing form instances.
 ;; note: indirect addressing form that use `[` & `]` need backslash in SBCL REPL,
 ;; eg: ULTRAELF> \[rax\]
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (loop for register-list in *create-addressing-form-instances-list*
-        do (loop for register-index below (length (second register-list))
-                 do (setf (symbol-value (intern (string-upcase (nth register-index (second register-list)))))
-                          (make-instance (third register-list)
-                                         :r/m (+ register-index (first register-list))
-                                         :name (string-upcase (nth register-index (second register-list))))))))
+(loop for register-list in *create-addressing-form-instances-list*
+      do (loop for register-index below (length (second register-list))
+               do (setf (symbol-value (intern (string-upcase (nth register-index (second register-list)))))
+                        (make-instance (third register-list)
+                                       :r/m (+ register-index (first register-list))
+                                       :name (string-upcase (nth register-index (second register-list)))))))
