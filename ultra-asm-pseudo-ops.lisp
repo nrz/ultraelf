cc9772f6d7d7d58fbb5a5ac5a37c75de1188ba01
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-01-28 23:31:23 +0200

    Poistettu käyttämättömiä argumentteja.
    Funktioihin voi tarvittaessa myöhemmin lisätä `&rest args`.
    
    	modified:   ultra-asm-pseudo-ops.lisp

--- ultra-asm-pseudo-ops.lisp
+++ ultra-asm-pseudo-ops.lisp
@@ -5,12 +5,12 @@
 
 (in-package :ultraelf)
 
-(defun bits-pseudo-op (arg1 &rest args)
+(defun bits-pseudo-op (arg1)
   "`bits` or `[bits` pseudo-op affects the encoding used. Use `[bits 64]` for x86-64."
   (defparameter *bits* arg1)
   nil)
 
-(defun align-pseudo-op (arg1 &rest args)
+(defun align-pseudo-op (arg1)
   "`align` aligns the code by emitting enough nop's (0x90)."
   (if (or
         (eql *global-offset* 0)
@@ -35,12 +35,12 @@
   "`dq` defines a quadword (8 bytes)."
   (apply #'append (loop for my-string in args collect (string-to-64-bit-little-endian my-string))))
 
-(defun global-pseudo-op (arg1 &rest args)
+(defun global-pseudo-op (arg1)
   "`global` pseudo-op defines the entry point (?) of the code."
   (defparameter *entry-point* arg1)
   nil)
 
-(defun section-pseudo-op (arg1 &rest args)
+(defun section-pseudo-op (arg1)
   "`section` pseudo-op starts a new section."
   (defparameter *current-section* arg1)
   nil)
