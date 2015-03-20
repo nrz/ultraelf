a55a55e81158492a848af9cadf429214ac14f5d7
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-03-20 22:42:34 +0200

    Muokattu kommentteja.
    	modified:   ultra-create-addressing-form-instances.lisp
    	modified:   ultra-instruction-classes.lisp

--- ultra-create-addressing-form-instances.lisp
+++ ultra-create-addressing-form-instances.lisp
@@ -22,14 +22,16 @@
 ;; design goals of ultraELF syntax:
 ;;  1. surjective function from assembly code to binary code (that is, every possible binary encoding has a distinct assembly syntax).
 ;;  2. injective non-surjective function from binary code to assemble code (that is, every possible binary encoding has a distinct assembly syntax).
-;;  3. possibility to hardcode displacements as constant values (useful for: 1. overlapping instructions, 2. code/data mixes).
-;;  4. possibility to split code into code blocks that can be reordered, the blocks usually ending with `ret` or `jmp?` (jump if the next block is not next).
-;;  5. possibility to use Common Lisp as macro language (every `(` in code opens a Common Lisp form that is `read` and `eval`ed).
-;;  6. possibility to define instruction-level constraints (for example: `(contains (list 0bff 0b00))` ), permitting the use of ultraELF regular expressions.
-;;  7. possibility to define embedded instructions recursively (overlapping instructions given as assembly code).
-;;  8. possibility to define code block-level constraints.
-;;  9. possibility to define program-level constraints.
-;; 10. maintain compatibility with NASM to the extent possible: ultraELF should assemble all NASM-syntax assembly code identical to NASM.
+;;  3. possibility to leave out all operand size where it is normally required, allowing several different encodings (eg. `mov [rsi],3`).
+;;  4. possibility to define more than 1 operand size, allowing several different encodings (eg. `mov [rsi], byte/word 3`).
+;;  5. possibility to hardcode displacements as constant values (useful for: 1. overlapping instructions, 2. code/data mixes).
+;;  6. possibility to split code into code blocks that can be reordered, the blocks usually ending with `ret` or `jmp?` (jump if the next block is not next).
+;;  7. possibility to use Common Lisp as macro language (every `(` in code opens a Common Lisp form that is `read` and `eval`ed).
+;;  8. possibility to define instruction-level constraints (for example: `(contains (list 0bff 0b00))` ), permitting the use of ultraELF regular expressions.
+;;  9. possibility to define embedded instructions recursively (overlapping instructions given as assembly code).
+;; 10. possibility to define code block-level constraints.
+;; 11. possibility to define program-level constraints.
+;; 12. maintain compatibility with NASM to the extent possible: ultraELF should assemble all valid NASM-syntax assembly code identical to NASM.
 ;;
 ;; keywords specific to ultraELF:
 ;; `pc`     program counter specific addressing (such as RIP-relative addressing) must be used.
@@ -61,3 +63,27 @@
 ;; `inc dword [++disp32 0]`       FF 04 25 00 00 00 00      FF /0       none, none, SS=01   inc dword [0x0]     SIB direct, displacement size 32 bits),disp. 0.
 ;; `inc dword [sib disp8 0]`      invalid                   n/a         n/a                 n/a                 invalid (there is no SIB direct with disp. size 8 bits.
 ;; `inc dword [++disp8 0]`        invalid                   n/a         n/a                 n/a                 invalid (there is no SIB direct with disp. size 8 bits.
+;; `inc dword [++disp32 0]`       SIB register indirect, displacement size 32 bits, displacement 0
+;; `inc dword [sib rax]`          SIB register indirect, rax as base
+;; `inc dword [sib rax+]`         SIB register indirect, rax as base
+;; `inc dword [sib rax++]`        SIB register indirect, rax as base, any displacement size, displacement 0
+;; `inc dword [sib +rax]`         SIB register indirect, rax as index
+;; `inc dword [sib +rax+]`        SIB register indirect, rax as index
+;; `inc dword [rax+]`             SIB register indirect, rax as base
+;; `inc dword [rax++0]`           SIB register indirect, rax as base, any displacement size, displacement 0
+;; `inc dword [rax++disp8 0]`     SIB register indirect, rax as base, displacement size 8 bits, displacement 0
+;; `inc dword [rax++disp32 0]`    SIB register indirect, rax as base, displacement size 32 bits, displacement 0
+;; `inc dword [sib +rax]`         SIB register indirect, rax as index
+;; `inc dword [+rax+]`            SIB register indirect, rax as index
+;; `inc dword [+rax+0]`           SIB register indirect, rax as index, any displacement size, displacement 0
+;; `inc dword [+rax+disp8 0]`     SIB register indirect, rax as index, displacement size 8 bits, displacement 0
+;; `inc dword [+rax+disp32 0]`    SIB register indirect, rax as index, displacement size 32 bits, displacement 0
+;; `inc dword [rax+disp]`         base, no SIB, uses disp8/disp32
+;; `inc dword [rax+disp 0]`       base, no SIB, uses disp8/disp32
+;; `inc dword [rax+disp8]`        base, no SIB, uses disp8
+;; `inc dword [rax+disp32]`       base, no SIB, uses disp8
+;; `inc dword [rax+]`             base, uses SIB
+;; `inc dword [0]`                basic direct, displacement size 32 bits, displacement 0
+;; `inc dword [+0]`               basic direct, displacement size 32 bits, displacement 0
+;; `inc dword [-0]`               basic direct, displacement size 32 bits, displacement 0
+;;
