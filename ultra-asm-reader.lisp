fc5d038c1ca0eb2a72fcd12d34cf6eb9e71c3bda
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-07-19 21:54:23 +0300

    Koodin siivousta: `(when (not` -> `(unless`.
    Ei toiminnallisia muutoksia.
    
    	modified:   ultra-asm-reader.lisp

--- ultra-asm-reader.lisp
+++ ultra-asm-reader.lisp
@@ -117,7 +117,7 @@
                       (setf my-string (concatenate 'string my-string my-char)))
                      ;; is character newline?
                      ((equal my-char (coerce (list #\Newline) 'string))
-                      (when (not (equal (get-last-character-string my-string) " "))
+                      (unless (equal (get-last-character-string my-string) " ")
                         ;; if last character was not space, output space.
                         (setf my-string (concatenate 'string my-string " "))))
                      ;; otherwise output the character.
