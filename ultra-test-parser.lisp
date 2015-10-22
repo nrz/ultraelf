08189eb22168ee4043b10d65e3a23a6bfc76bd97
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-10-23 02:32:40 +0300

    `defun test-parser`: Poistettu tarpeettomia testejä.
    Lisäksi muokattu olemassaolevia testejä niin, että käskyn jälkeen tulee
    aina välilyöntejä, ei pilkkua.
    
    Pilkun ei tarvitse toimia käskyn ja parametrien erottimena, riittää
    että se toimii parametrien erottamiseen toisistaan (välilyönti käy
    myös).
    
    	modified:   ultra-test-parser.lisp

--- ultra-test-parser.lisp
+++ ultra-test-parser.lisp
@@ -12,14 +12,11 @@
   ;; Tests for basic syntax tree creation.
   (rt:deftest test-create-syntax-tree-foo (create-syntax-tree #a foo #e) (list '("foo")))
   (rt:deftest test-create-syntax-tree-foo-bar (create-syntax-tree #a foo bar #e) (list '("foo" "bar")))
-  (rt:deftest |test-create-syntax-tree-foo,bar| (create-syntax-tree #a foo,bar #e) (list '("foo" "bar")))
   (rt:deftest |test-create-syntax-tree-foo-bar,baz| (create-syntax-tree #a foo bar,baz #e) (list '("foo" "bar" "baz")))
-  (rt:deftest |test-create-syntax-tree-foo,bar-baz| (create-syntax-tree #a foo,bar baz #e) (list '("foo" "bar" "baz")))
-  (rt:deftest |test-create-syntax-tree-foo,bar,baz| (create-syntax-tree #a foo,bar,baz #e) (list '("foo" "bar" "baz")))
   (rt:deftest |test-create-syntax-tree-foo1-bar1-foo2-bar2| (create-syntax-tree #a foo1 bar1 #a foo2 bar2 #e) (list '("foo1" "bar1") '("foo2" "bar2")))
-  (rt:deftest |test-create-syntax-tree-foo1,bar1-foo2,bar2-foo3,bar3| (create-syntax-tree #a foo1,bar1 #a foo2,bar2 #a foo3,bar3 #e) (list '("foo1" "bar1") '("foo2" "bar2") '("foo3" "bar3")))
-  (rt:deftest |test-create-syntax-tree-foo1-[bar1-bar2]| (create-syntax-tree #a foo [bar1 bar2] #e) (list '("foo1" "[bar1 bar2]")))
-  (rt:deftest |test-create-syntax-tree-foo1,[bar1-bar2-bar3]| (create-syntax-tree #a foo [bar1 bar2 bar3] #e) (list '("foo1" "[bar1 bar2 bar3]")))
+  (rt:deftest |test-create-syntax-tree-foo1-bar1-foo2-bar2-foo3-bar3| (create-syntax-tree #a foo1 bar1 #a foo2 bar2 #a foo3 bar3 #e) (list '("foo1" "bar1") '("foo2" "bar2") '("foo3" "bar3")))
+  (rt:deftest |test-create-syntax-tree-foo-[bar1-bar2]| (create-syntax-tree #a foo [bar1 bar2] #e) (list '("foo" "[bar1 bar2]")))
+  (rt:deftest |test-create-syntax-tree-foo,[bar1-bar2-bar3]| (create-syntax-tree #a foo [bar1 bar2 bar3] #e) (list '("foo" "[bar1 bar2 bar3]")))
 
   ;; Tests for processing of memory address syntax.
 
