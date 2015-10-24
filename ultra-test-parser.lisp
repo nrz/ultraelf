5764fb7e73d646b2b13fe2d37bcb5982a74905dd
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-10-24 19:39:30 +0300

    Järjestelty testejä ja korjattu yhden testin virheellinen nimi.
    Kaikki parseritestit menevät läpi.
    
    	modified:   ultra-test-parser.lisp

--- ultra-test-parser.lisp
+++ ultra-test-parser.lisp
@@ -19,10 +19,6 @@
   (rt:deftest |test-create-syntax-tree-foo-bar,more-extra-space-baz| (create-syntax-tree #a foo bar,  baz #e) (list '("foo" "bar" "baz")))
   (rt:deftest |test-create-syntax-tree-foo1-bar1-foo2-bar2| (create-syntax-tree #a foo1 bar1 #a foo2 bar2 #e) (list '("foo1" "bar1") '("foo2" "bar2")))
   (rt:deftest |test-create-syntax-tree-foo1-bar1-foo2-bar2-foo3-bar3| (create-syntax-tree #a foo1 bar1 #a foo2 bar2 #a foo3 bar3 #e) (list '("foo1" "bar1") '("foo2" "bar2") '("foo3" "bar3")))
-  (rt:deftest |test-create-syntax-tree-foo-[bar1-bar2]| (create-syntax-tree #a foo [bar1 bar2] #e) (list '("foo" "[bar1 bar2]")))
-  (rt:deftest |test-create-syntax-tree-foo,[bar1-bar2-bar3]| (create-syntax-tree #a foo [bar1 bar2 bar3] #e) (list '("foo" "[bar1 bar2 bar3]")))
-  (rt:deftest |test-create-syntax-tree-foo-[bar1-extra-space-bar2]| (create-syntax-tree #a foo [bar1  bar2] #e) (list '("foo" "[bar1 bar2]")))
-  (rt:deftest |test-create-syntax-tree-foo-[bar1-more-extra-space-bar2]| (create-syntax-tree #a foo [bar1   bar2] #e) (list '("foo" "[bar1 bar2]")))
 
   ;; Tests for syntax tree creation using assembly source code with Common Lisp macros.
   (rt:deftest test-create-syntax-tree-hash-l-foo (create-syntax-tree #a #l "foo" #e) (list '("foo")))
@@ -36,5 +32,9 @@
   (rt:deftest test-code-with-lisp-number-3 (create-syntax-tree *test-code-with-lisp-number-3*) (list '("foo1" "bar1") '("foo2" "bar2") '("foo3" "bar3") '("foo4" "bar4") '("foo5" "bar5")))
 
   ;; Tests for processing of memory address syntax.
+  (rt:deftest |test-create-syntax-tree-foo-[bar1-bar2]| (create-syntax-tree #a foo [bar1 bar2] #e) (list '("foo" "[bar1 bar2]")))
+  (rt:deftest |test-create-syntax-tree-foo-[bar1-bar2-bar3]| (create-syntax-tree #a foo [bar1 bar2 bar3] #e) (list '("foo" "[bar1 bar2 bar3]")))
+  (rt:deftest |test-create-syntax-tree-foo-[bar1-extra-space-bar2]| (create-syntax-tree #a foo [bar1  bar2] #e) (list '("foo" "[bar1 bar2]")))
+  (rt:deftest |test-create-syntax-tree-foo-[bar1-more-extra-space-bar2]| (create-syntax-tree #a foo [bar1   bar2] #e) (list '("foo" "[bar1 bar2]")))
 
   (rt:do-tests))
