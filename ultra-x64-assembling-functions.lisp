1226d35b8afe2f3943d5d8d7dac7799eb7d24c40
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-09-04 01:14:56 +0300

    `handle-nasm-code-format-x64-wrapper`: `given-operands` is not `&key`.
    All tests which passed before pass still.
    
    	modified:   ultra-x64-assembling-functions.lisp

--- ultra-x64-assembling-functions.lisp
+++ ultra-x64-assembling-functions.lisp
@@ -64,8 +64,8 @@
 (defun handle-nasm-code-format-x64-wrapper
   (code-format
     req-operands
-    &key
     given-operands
+    &key
     encoded-bytes
     is-rex-already-encoded
     (instruction-length-in-bytes 0))
@@ -221,7 +221,7 @@
                            (handle-nasm-code-format-x64-wrapper
                              (cons encoding-type (nthcdr 2 code-format)) ; `code-format`
                              req-operands
-                             :given-operands given-operands
+                             given-operands
                              :encoded-bytes (append
                                               encoded-bytes
                                               (list
@@ -820,49 +820,49 @@
       ((equal encoding-type "[")
        ;; The encoding of this variant is constant, so just convert
        ;; the rest elements (hexadecimal numbers) to numbers in a list.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands nil))
       ((equal encoding-type "[--:")
        ;; The operands and encoding of this variant are fixed.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[-i:")
        ;; One fixed operand and one immediate operand.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[-r:")
        ;; One fixed operand and one register operand.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[m:")
        ;; This variant has one 'memory' (can be register too) operand.
        ;; The operand is encoded in the r/m field.
        ;; An extension of the opcode is in the reg field.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[i:")
        ;; This variant has one immediate operand.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[r:")
        ;; This variant has one register operand.
        ;; The operand is encoded in the r/m field.
        ;; An extension of the opcode is in the reg field.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[i-:")
        ;; One immediate operand and one fixed operand.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[r-:")
        ;; One register operand and one fixed operand.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[mi:")
        ;; One memory operand and one immediate operand.
        ;; The operands are encoded in corresponding ModRM fields.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[mr:")
        ;; One memory operand and one register operand.
        ;; The operands are encoded in corresponding ModRM fields.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[ri:")
        ;; One register operand and one immediate operand.
        ;; The operands are encoded in corresponding ModRM fields.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       ((equal encoding-type "[rm:")
        ;; One register operand and one memory operand.
        ;; The operands are encoded in corresponding ModRM fields.
-       (handle-nasm-code-format-x64-wrapper code-format my-operands :given-operands given-operands))
+       (handle-nasm-code-format-x64-wrapper code-format my-operands given-operands))
       (t (error (concatenate 'string "encoding type " encoding-type " not yet implemented"))))))
